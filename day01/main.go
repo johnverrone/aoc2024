0dcf7622377662359400517f6725c34bdd2b5079
John Verrone <john.verrone@gmail.com>
2024-11-15 18:59:58 -0700

    2024 setup

--- day01/main.go
+++ day01/main.go
@@ -1,12 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const sample = `
+12
+14
+1969
+100756
+`
+
+// this is 2019's day 1, just getting setup for 2024 with Golang
 
 func main() {
-	b := Bar()
-	fmt.Println("Hello, World!", b)
+	sum := 0
+	lines := strings.Split(sample, "\n")
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+		n := mustInt(line)
+		for {
+			n = n/3 - 2
+			if n > 0 {
+				sum += n
+			} else {
+				break
+			}
+		}
+	}
+	fmt.Println(sum)
 }
 
-func Bar() string {
-	return "Bar"
+func mustInt(s string) int {
+	if v, err := strconv.Atoi(s); err != nil {
+		panic(fmt.Sprint(s, err))
+	} else {
+		return v
+	}
 }
